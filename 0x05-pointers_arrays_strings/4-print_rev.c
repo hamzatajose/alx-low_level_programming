76b131c1d66b8aef31a7f2fcda7ffef3b9d76940
hamzatajose <niyiajose6@gmail.com>
2022-12-19 20:32:08 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_rev - prints a string to standard output in reverse form
+ *
+ *   * @s : the string variable to be printed
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_rev(char *s)
+
+{
+
+		int x;
+
+
+
+			int c = 0;
+
+
+
+				while (s[c] != '\0')
+
+						{
+
+									c++;
+
+										}
+
+					x = c - 1;
+
+
+
+						while (x >= 0)
+
+								{
+
+											_putchar(s[x]);
+
+													x--;
+
+														}
+
+							_putchar('\n');
+
+}
