53ae6b8bff59e926f6705b5c5ff1d871a18592c8
hamzatajose <niyiajose6@gmail.com>
2022-12-23 02:21:03 -0800

    Write more functions with c arrays, strings and pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,16 @@
-
-
 #include "main.h"
-
 #include <string.h>
-
 /**
+ * _strcat - appends the src string to the dest string, overwriting
+ * the terminating null byte (\0) at the end of dest, and then adds
+ * a terminating null byte
  *
- *  * _strcat - appends the src string to the dest string, overwriting
+ * @dest: the destination pointer
+ * @src: the source pointer
  *
- *   * the terminating null byte (\0) at the end of dest, and then adds
- *
- *    * a terminating null byte
- *
- *     *
- *
- *      * @dest: the destination pointer
- *
- *       * @src: the source pointer
- *
- *        *
- *
- *         * Return: destination pointer
- *
- *          */
-
+ * Return: destination pointer
+ */
 char *_strcat(char *dest, char *src)
-
 {
-
-		return (strcat(dest, src));
-
+	return (strcat(dest, src));
 }
